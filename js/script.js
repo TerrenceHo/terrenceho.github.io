a6e9ebabf5969c1d4e2695fd92e5fb6e64f2ef92
[Terrence Ho] <[terrenceho.books@gmail.com]>
2016-11-23 00:38:37 -0800

    [Added Bear SVG.  Need to connect them into one loop.]

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
-  var drawFace = new Vivus('drawFaceImg', {type: 'delayed', duration: 220, delay: 180, start: 'autostart'});
+	var d = 1;
+    var e = 1;
+    var drawBear = new Vivus('drawBearImg', {type:'delayed', duration: 400, delay: 180,start:'autostart'},
+    function(c){
+        d *= -1;
+        if(d == 1 && e == 1){
+            setTimeout(function(){
+                drawBear.play(d)
+            },800);
+	    }
+        else if(d == -1 && e == 1){
+            drawBear.play(d);
+    	}
+    });
+    
+  	var drawFace = new Vivus('drawFaceImg', {type: 'delayed', duration: 400, delay: 180, start: 'autostart'}, function(c){
+        d *= -1;
+        if(d == 1 && e == 1){
+            setTimeout(function(){
+                drawFace.play(d)
+            },800);
+	    }
+        else if(d == -1 && e == 1){
+            drawFace.play(d);
+    	}
+        else if(d == 1 && e == -1){
+            setTimeout(function(){
+            drawBear.play(d)
+            },800);
+        }
+        else if (d==-1 && e == -1){
+            drawBear.play(d);
+        }
+  	});
 });
