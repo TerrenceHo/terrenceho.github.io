74c3a8a7c8405742643d229a5d1b1fedccd47536
[Terrence Ho] <[terrenceho.books@gmail.com]>
2016-12-20 12:16:32 -0800

    [Added Holiday Special]

--- js/script.js
+++ js/script.js
@@ -1,73 +1,3 @@
-//$(document).ready(function(){
-//    var e = 1;
-//	var d = -1;
-//    
-////    var drawBear = new Vivus('drawBearImg', {type:'delayed', duration: 300, delay:180});
-////    drawBear.stop();
-////    drawBear.reset();
-////    drawBear.play(1,function(){
-////        drawBear.play(-1);
-////    });
-////    drawBear.finish();
-////    drawBear.play(-1);
-//    var drawBear = new Vivus('drawBearImg', {type:'delayed', duration: 300, delay:180,start:'manual'});
-//    
-//  	var drawFace = new Vivus('drawFaceImg', {type:'delayed', duration:300, delay:180, start: 'manual'}); 
-//    
-//    setTimeout(function(){
-//        drawFace.play();
-//    }, 3000);
-//    drawFace.stop();
-////    drawFace.play(d);
-//});
-
-
-
-
-//
-//$(document).ready(function(){
-//	var d = 1;
-//    var e = 1;
-//    var drawBear = new Vivus('drawBearImg', {type:'delayed', duration: 300, delay:180,start:'manual'}, function(){
-//        console.log("drawing bear");
-//    });
-//    
-//  	var drawFace = new Vivus('drawFaceImg', {type:'delayed', duration:300, delay:180, start: 'autostart'}, function(c){
-//        d *= -1;
-//        console.log("First d = " + d);
-//        if(d === 1 && e === 1){
-//            setTimeout(function(){
-//                drawFace.play(d);
-//            },800);
-//            console.log("d = " + d);
-//            console.log("e = " + e);
-//	    }
-//        else if(d === -1 && e === 1){
-//            drawFace.play(d);
-//            console.log("d = " + d);
-//            console.log("e = " + e);
-//            e *= -1;
-//    	}  
-//        else if ( d === 1 && e === -1){
-//            setTimeout(function(){
-//                drawBear.play(d);
-//            },800);
-//            console.log("drawing bear");
-//            console.log("d = " + d);
-//            console.log("e = " + e);
-//        }
-//        else if(d === -1 && e === -1){
-//            drawBear.play(d);
-//            console.log("d = " + d);
-//            console.log("e = " + e);
-//            e *= -1;
-//        }
-//  	});
-//});
-
-
-
-
 $(document).ready(function(){
 	var d = 1;
     var e = 1;
@@ -103,37 +33,8 @@ $(document).ready(function(){
             },800);
         }
   	});
+    
+    var drawHoliday = new Vivus('drawHolidayImg', {type:'delayed', duration:300, delay:180, start:'autostart'});
 });
 
 
-//$(document).ready(function(){
-//	var d = 1;
-//    var drawBear = new Vivus('drawBearImg', {start:'autostart', speed:300}, function(c){
-//        d *= -1;
-//        if(d == 1){
-//            setTimeout(function(){
-//                drawBear.play(d)
-//            },800);
-//            console.log(d);
-//	    }
-//        else if(d == -1){
-//            drawBear.play(d);
-//            console.log(d);
-//    	}
-//    });
-//    
-//  	var drawFace = new Vivus('drawFaceImg', {start:'autostart', speed:300}, function(c){
-//        d *= -1;
-//        if(d == 1){
-//            setTimeout(function(){
-//                drawFace.play(d)
-//            },800);
-//            console.log(d);
-//	    }
-//        else if(d == -1){
-//            drawFace.play(d);
-//    	}
-//  	});
-//    
-//});
-//
---
+++ special/holidayScript.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    var drawHoliday = new Vivus('drawHolidayImg', {type:'delayed', duration:300, delay:180, start:'autostart'});
+});
